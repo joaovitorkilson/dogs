b880702911141d8be7e1cb38a1993f636c2218f3
joaovitorkilson <joaovitorkilson@gmail.com>
2025-03-31 16:43:51 -0300

    adicionado hook customizado useForm

--- src/Components/Forms/Input.jsx
+++ src/Components/Forms/Input.jsx
@@ -1,13 +1,24 @@
-import React from 'react'
-import styles from './Input.module.css'
-function Input({ label, type, name }) {
+import React from "react";
+import styles from "./Input.module.css";
+
+function Input({ label, type, name, value, onChange, error, onBlur }) {
   return (
     <div className={styles.wrapper}>
-      <label htmlFor={name} className={styles.label}>{label}</label>
-      <input id={name} name={name} className={styles.input} type={type}/>
-      <p className={styles.error}>Error</p>
+      <label htmlFor={name} className={styles.label}>
+        {label}
+      </label>
+      <input
+        id={name}
+        name={name}
+        className={styles.input}
+        type={type}
+        value={value}
+        onChange={onChange}
+        onBlur={onBlur}
+      />
+      {error && <p className={styles.error}>{error}</p>}
     </div>
-  )
+  );
 }
 
-export default Input
+export default Input;
