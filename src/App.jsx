fb83b1908767a4d4eee50f56e55c09d1b0287351
joaovitorkilson <joaovitorkilson@gmail.com>
2025-03-31 17:57:18 -0300

    adicionado useContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,26 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Header from './Components/Header'
-import Footer from './Components/Footer'
-import Home from './Components/Home'
-import Login from './Components/Login/Login'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import Home from "./Components/Home";
+import Login from "./Components/Login/Login";
+import { UserStorage } from "./UserContext";
 
 function App() {
-
   return (
     <>
       <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login/*" element={<Login />} />
-        </Routes>
-        <Footer />
+        <UserStorage>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login/*" element={<Login />} />
+          </Routes>
+          <Footer />
+        </UserStorage>
       </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,19 +1,29 @@
-import React from 'react'
-import styles from './Header.module.css'
-import { Link } from 'react-router-dom'
-import Dog from '../Assets/dogs.svg?react';
+import React from "react";
+import styles from "./Header.module.css";
+import { Link } from "react-router-dom";
+import Dog from "../Assets/dogs.svg?react";
+import { UserContext } from "../UserContext";
 
 function Header() {
+  const { data } = React.useContext(UserContext);
   return (
     <header className={styles.header}>
-        <nav className={`${styles.nav} container`}>
-            <Link className={styles.logo} to="/" aria-label='Dogs - Home'>
-              <Dog />
-            </Link>
-            <Link className={styles.login} to="/login">Login / Criar</Link>
-        </nav>
+      <nav className={`${styles.nav} container`}>
+        <Link className={styles.logo} to="/" aria-label="Dogs - Home">
+          <Dog />
+        </Link>
+        {data ? (
+          <Link className={styles.login} to="/conta">
+            {data.nome}
+          </Link>
+        ) : (
+          <Link className={styles.login} to="/login">
+            Login / Criar
+          </Link>
+        )}
+      </nav>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
