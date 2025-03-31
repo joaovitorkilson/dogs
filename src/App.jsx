96548a6f8c05f80824c37735d2aef8968f6d763a
joaovitorkilson <joaovitorkilson@gmail.com>
2025-03-31 14:38:02 -0300

    adicionado componentes footer e header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Header from './Components/Header'
+import Footer from './Components/Footer'
+import { BrowserRouter } from 'react-router-dom'
 
 function App() {
 
   return (
     <>
+      <Header />
+      <Footer />
     </>
   )
 }
