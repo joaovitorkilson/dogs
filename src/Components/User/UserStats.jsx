f3dc64ca0be80bc4701babc7c75adacfbc0e174c
joaovitorkilson <joaovitorkilson@gmail.com>
2025-04-10 20:28:12 -0300

    adicionado seção statísticas

--- src/Components/User/UserStats.jsx
+++ src/Components/User/UserStats.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React from "react";
+import useFetch from "../../Hooks/useFetch";
+import { GET_STATS } from "../../../Api";
+import Loading from "../Helpers/Loading";
+import Error from "../Helpers/Error";
+
+const UserStatsGraphs = React.lazy(() => import('./UserStatsGraphs'))
 
 const UserStats = () => {
-  return (
-    <div>
-      estatisticas
-    </div>
-  )
-}
+  const { data, error, loading, request } = useFetch();
 
-export default UserStats
+  React.useEffect(() => {
+    async function getData() {
+      const { url, options } = GET_STATS();
+      await request(url, options);
+    }
+    getData();
+  }, [request]);
+
+  if (loading) return <Loading />;
+  if (error) return <Error error={error} />;
+  if (data) {
+    return (
+      <React.Suspense fallback={<div></div>}>
+        <UserStatsGraphs data={data} />
+      </React.Suspense>
+    );
+  } else {
+    return null;
+  }
+};
+
+export default UserStats;
